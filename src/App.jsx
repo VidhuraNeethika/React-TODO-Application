de6272263ce175468c8355d4aae4145f0644ef75
VidhuraNeethika <vidhuraneethika000@gmail.com>
2024-04-17 13:05:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+const App = () => {
+
+    const [getX, setX] = useState(0);
+
+    const btnClick = () => {
+        setX(getX + 1);
+    }
+
+    return (
+        <div>
+
+            {getX}
+
+            <button onClick={() => {
+                btnClick()
+            }}>Click Me
+            </button>
+
+        </div>
+    )
+}
+export default App
